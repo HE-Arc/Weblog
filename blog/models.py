1bbb7e2cc37f5f2e0ba2ffae1e50ecc5f41be90f
lancelot <lancelotlancelot94@gmail.com>
2017-02-22 16:02:19 +0100

    models update, 'first try'

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Article(models.Model):
+    title = models.CharField(max_length=134)
+    date = models.DateField()
+    body = models.CharField()
+    # category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    # author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    # tags=models.ForeignKey(Tag,on_delete=models.CASCADE)# usefull?
+    slug = models.AutoField(primary_key=True)
