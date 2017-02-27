f3f4e214f5b1fd03be061e720249f9aa8aeea84f
lancelot <lancelotlancelot94@gmail.com>
2017-02-27 13:41:27 +0100

    models and migration 1st try

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,30 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(primary_key=True, max_length=50)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    # infos: stuff about user
+    infos = models.CharField(max_length=1000)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
 
 class Article(models.Model):
     title = models.CharField(max_length=134)
     date = models.DateField()
-    body = models.CharField()
-    # category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    # author=models.ForeignKey(Author,on_delete=models.CASCADE)
-    # tags=models.ForeignKey(Tag,on_delete=models.CASCADE)# usefull?
-    slug = models.AutoField(primary_key=True)
+    body = models.TextField()
+    categories = models.ManyToManyField(Category)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    slug = models.SlugField(primary_key=True)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=30)
+    content = models.CharField(max_length=1000)
+    email = models.EmailField()
+    date = models.DateField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
