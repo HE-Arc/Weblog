f6ff5e090662f5b20798ee297e49e35937e4f843
lancelot <lancelotlancelot94@gmail.com>
2017-03-22 15:15:54 +0100

    auth and stuffs (?)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+
+from django.conf.urls import url,include
 from blog.views import Index
 from . import views
 
+
 urlpatterns = [
     url(r'^$', Index.as_view()),
+    url('^', include('django.contrib.auth.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Article
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views import View
+# from .forms import AuthForm
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -13,3 +16,23 @@ class Index(View):
         return render(request, 'blog/index.html', {'articles': articles})
 
 
+# class AdminAuth(View):
+#     def get(self, request):
+#         form = AuthForm(request.POST)
+#         if form.is_valid():
+#             return HttpResponseRedirect('/thanks/')
+#         else:
+#             form = AuthForm()
+#
+#         return render(request, 'blog/login.html', {'form': form})
+
+class AuthView(View):
+    def get(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            articles = Article.objects.filter(date__lte=timezone.now()).order_by('date')
+            return render(request, 'blog/index.html', {'articles': articles})
