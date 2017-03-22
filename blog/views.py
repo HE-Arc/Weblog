224d1980e74093eb25f3c736c6350b14e44d1b4d
lancelot <lancelotlancelot94@gmail.com>
2017-03-22 13:02:45 +0100

    add __str__ to models

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,9 @@ from django.utils import timezone
 from .models import Article
 
 
-
 # Create your views here.
 def index(request):
-	
-	#Tri les articles selon la date de publication
-	articles = Article.objects.filter(date__lte=timezone.now()).order_by('date')
-    
-	return render(request, 'blog/index.html', {'articles': articles})
+    # Tri les articles selon la date de publication
+    articles = Article.objects.filter(date__lte=timezone.now()).order_by('date')
+
+    return render(request, 'blog/index.html', {'articles': articles})
