7dfee9daad5540c12af77c10c6e55fb0823aabcc
lancelot <lancelotlancelot94@gmail.com>
2017-04-18 15:27:05 +0200

    acces to newArticle route restriction

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from blog.views import Index, ArticleNewForm, ArticleView
 from . import views
-
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name="index"),
-    url(r'^newArticle/$', ArticleNewForm.as_view()),
+    url(r'^newArticle/$', login_required(ArticleNewForm.as_view())),
     url('^', include('django.contrib.auth.urls')),
     url(r'^markdownx/', include('markdownx.urls')),
 	url(r'^article/(?P<slug>[\w-]+)/$', ArticleView.as_view()),
